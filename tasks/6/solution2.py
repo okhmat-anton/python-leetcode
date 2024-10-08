fabc9f5f30a4d13756e30792d62d69b2aef23cc0
boss <okhmat.anton@icloud.com>
2024-10-07 21:42:08 -0700

    4

--- tasks/6/solution2.py
+++ tasks/6/solution2.py
@@ -3,20 +3,19 @@ class Solution(object):
         result = 0
         c = len(s) - 1
         letters = {
-            'I':1,
-            'V':5,
-            'X':10,
-            'L':50,
-            'C':100,
-            'D':500,
-            'M':1000
+            'I': 1,
+            'V': 5,
+            'X': 10,
+            'L': 50,
+            'C': 100,
+            'D': 500,
+            'M': 1000
         }
         while c >= 0:
-            if s[c] in letters:
-                result += letters[s[c]]
-                while c > 0 and s[c] in letters and letters[s[c]] > letters[s[c-1]]:
-                    result -= letters[s[c-1]]
-                    c -= 1
+            result += letters[s[c]]
+            while c > 0 and letters[s[c]] > letters[s[c-1]]:
+                result -= letters[s[c-1]]
+                c -= 1
             c -= 1
         return result
 
